3e77728e8e85fe1b3104db86f6afd7f38ac11bd3
developer2520 <berkino2099@gmail.com>
2024-05-09 16:47:07 +0500

    hello

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {store} from './store.js'
+import {store} from './app/store.js'
 import { Provider } from 'react-redux'
 
 
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <Provider store={store}>
     <App />
-    </Provider>
+    </Provider> 
     
   </React.StrictMode>,
 )
